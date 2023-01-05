36a5e3fa3059044a6ded1fb0795dc9f6e9dd69c7
yonasketema <yonask.kitaw@gmail.com>
2023-01-05 22:44:05 +0300

    api: create food |getnearby foods

---
+++ controllers/foodController.js
@@ -0,0 +1,65 @@
+const Food = require("../models/food");
+const Restaurant = require("../models/restaurant");
+
+exports.postFood = async (req, res) => {
+  try {
+    const food = await Food.create(req.body);
+
+    res.status(201).json({
+      status: "success",
+      food,
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: "fial",
+      error: error.message,
+    });
+  }
+};
+
+exports.getNearbyFood = async (req, res) => {
+  const { lat, log, foodname } = req.query;
+
+  console.table({ lat, log, foodname });
+
+  try {
+    const foods = await Restaurant.aggregate([
+      {
+        $geoNear: {
+          near: { type: "Point", coordinates: [+lat, +log] },
+          distanceField: "dist",
+          maxDistance: 100,
+          key: "location",
+          spherical: true,
+        },
+      },
+      {
+        $lookup: {
+          from: "foods",
+          localField: "_id",
+          foreignField: "restaurant",
+          as: "menu",
+        },
+      },
+
+      {
+        $unwind: "$menu",
+      },
+      {
+        $match: {
+          "menu.name": foodname,
+        },
+      },
+    ]);
+
+    res.status(200).json({
+      status: "success",
+      foods,
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: "fial",
+      error: error.message,
+    });
+  }
+};
