305e05b0a713597b54589c40830faf2caf86a3b1
yonasketema <yonask.kitaw@gmail.com>
2023-12-31 11:02:51 +0300

    refactor : user profile  resposne

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -3,10 +3,5 @@ const User = require("../models/userModel");
 exports.me = async (req, res) => {
   const user = await User.findById(req.user.id);
 
-  res.status(201).json({
-    status: "success",
-    data: {
-      user,
-    },
-  });
+  res.status(200).json(user);
 };
